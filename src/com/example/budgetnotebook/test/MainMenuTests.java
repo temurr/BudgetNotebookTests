2c9f06ba11a35c8494164a72f70c69767c5ab2e5
Tricia Murray <trishbraun@gmail.com>
2014-07-26 11:19:49 -0700

    Added more tests

--- src/com/example/budgetnotebook/test/MainMenuTests.java
+++ src/com/example/budgetnotebook/test/MainMenuTests.java
@@ -2,7 +2,9 @@ package com.example.budgetnotebook.test;
 
 import junit.framework.TestCase;
 import android.accounts.Account;
+import android.content.Intent;
 import android.os.SystemClock;
+import android.sax.StartElementListener;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -45,11 +47,69 @@ import com.example.budgetnotebook.Transaction;
 		@SmallTest
 		public void testViewAccountsClick() {
 			// Test the ACCOUNT activity is started when VIEW ACCOUNTS is clicked
+			Intent launchIntent = new Intent(getInstrumentation().getTargetContext(), activity.getClass());
+			activity.startActivity(launchIntent);
+
 			VIEW_ACCOUNTS = (Button) activity.findViewById(com.example.budgetnotebook.R.id.view_account);
 			TouchUtils.clickView(this, VIEW_ACCOUNTS);
 			SystemClock.sleep(1000);
 			super.assertEquals("Activity is of wrong type", MainMenu.class, activity.getClass());
 		}
+		
+		@SmallTest
+		public void testAddTransactionClick() {
+			// Test the ADD TRANSACTION activity is started when ADD TRANSACTION is clicked
+			Intent launchIntent = new Intent(getInstrumentation().getTargetContext(), activity.getClass());
+			activity.startActivity(launchIntent);
+
+			ADD_TRANSACTION = (Button)activity.findViewById(com.example.budgetnotebook.R.id.add_transaction);
+			TouchUtils.clickView(this,  ADD_TRANSACTION);
+			SystemClock.sleep(1000);
+			super.assertEquals("Activity is of wrong type",  MainMenu.class, activity.getClass());
+		}
+		
+		@SmallTest
+		public void testMyProfileClick() {
+			Intent launchIntent = new Intent(getInstrumentation().getTargetContext(), activity.getClass());
+			activity.startActivity(launchIntent);
+
+			// Test the MY PROFILE activity is started when MY PROFILE is clicked
+			MY_PROFILE = (Button)activity.findViewById(com.example.budgetnotebook.R.id.view_profile);
+			TouchUtils.clickView(this, MY_PROFILE);
+			SystemClock.sleep(1000);
+			super.assertEquals("Activity is of wrong type", MainMenu.class, activity.getClass());
+		}
+		
+		@SmallTest
+		public void testViewGoalsClick() {
+			Intent launchIntent = new Intent(getInstrumentation().getTargetContext(), activity.getClass());
+			activity.startActivity(launchIntent);
+
+			// Test the VIEW GOALS activity is started when VIEW GOALS is clicked
+			VIEW_GOALS = (Button)activity.findViewById(com.example.budgetnotebook.R.id.view_goals);
+			TouchUtils.clickView(this,  VIEW_GOALS);
+			SystemClock.sleep(1000);
+			super.assertEquals("Activity is of wrong type", MainMenu.class, activity.getClass());
+		}
+		
+		/*
+		public void testAdvancedMenuClick(){
+			Intent launchIntent = new Intent(getInstrumentation().getTargetContext(), activity.getClass());
+			activity.startActivity(launchIntent);
+
+			// Test the ADVANCED MENU activity is started when ADVANCED MENU is clicked
+			ADVANCED_MENU = (Button)activity.findViewById(com.example.budgetnotebook.R.id.view_recommendations);
+			TouchUtils.clickView(this, ADVANCED_MENU);
+			SystemClock.sleep(1000);
+			super.assertEquals("Activity is of wrong type", MainMenu.class, activity.getClass());
+		}*/
+		
+		/*public void testExitClick() {
+			EXIT = (Button)activity.findViewById(com.example.budgetnotebook.R.id.exit);
+			TouchUtils.clickView(this, EXIT);
+			SystemClock.sleep(1000);
+			// Not sure what to test for here
+		}*/
 
 		@Override
 		protected void tearDown() throws Exception {
